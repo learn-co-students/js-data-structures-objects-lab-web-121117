8516f665d083f6d89e2ae0967a9af10e240155d1
brianabaker <brianaclairbaker@gmail.com>
2018-01-29 13:04:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+
+const driver = {};
+
+// The function should not mutate the `driver` parameter and return a new `driver` that has an updated `value` for the `key` passed in
+
+function updateDriverWithKeyAndValue(obj, key, value){
+  const newObj = {... obj};
+  // the spread operator
+  // copies all of the old object's menu properties into the new object
+  newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromDriverByKey(obj, key){
+  newObj = {};
+  Object.assign(newObj, obj);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key){
+  delete obj[key];
+  return obj;
+}
